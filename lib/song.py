4e38ed9a6c15508d25ead7e24e70976becd74a02
Ndisyamwende <sharonndisya2002@gmail.com>
2024-03-05 14:32:53 +0300

    passed tests

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,27 @@
 class Song:
-    pass
+    count = 0
+    artists = set()
+    genres = set()
+    genre_count = {}
+    artist_count = {}
+
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1 #the ++shows increament when new song is created
+        Song.artists.add(artist)
+        Song.genres.add(genre)
+
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+
+    @classmethod
+    def get_genre_count(cls):
+        return cls.genre_count
+    
+    @classmethod
+    def get_artist_count(cls):
+        return cls.artist_count
